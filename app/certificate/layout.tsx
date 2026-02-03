d76a1fcc328728944a8a6d2ac78ae8c17f2e65fc
ROHIT_YERUNKAR_08 <rohityerunkar1508@gmail.com>
2026-02-03 16:42:27 +0530

    Delete app directory

--- app/certificate/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-export default function CertificateLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Suspense fallback={<div>Loading certificate...</div>}>{children}</Suspense>;
-}
