4bc991d94a1780b137520f323b67e0855701f952
ROHIT_YERUNKAR_08 <rohityerunkar1508@gmail.com>
2026-02-04 16:46:50 +0530

    Update CertificateContent.tsx

--- app/certificate/CertificateContent.tsx
+++ app/certificate/CertificateContent.tsx
@@ -139,7 +139,7 @@ export default function CertificateContent() {
           {/* Name Overlay - Positioned on the line */}
           <div style={{
             position: "absolute",
-            top: "37%",
+            top: "39%",
             left: "0",
             right: "0",
             transform: "translateY(-50%)",
